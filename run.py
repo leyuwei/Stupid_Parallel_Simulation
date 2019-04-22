9e9c72809b52ac9c48f2930066d38e0a9dd2a817
LeYuwei <leyuwei126@126.com>
2019-04-22 18:17:10 +0800

    0422 Update

--- run.py
+++ run.py
@@ -14,7 +14,7 @@ def main():
 	print("For each set of simulation parameter, we help you start a new instance to run it.")
 	print("Though stupid, we still coded this script to help you finish your work before deadline!")
 	print("We still hope that you make some modifications to your stupid code to fully utilize your multi-core CPU!")
-	print("This script can only support WINDOWS environment and PYTHON projects for the current version!")
+	print("This script supports WINDOWS environment and Python/Matlab projects!")
 	input("PRESS ANY KEY when ready...")
 
 	print()
@@ -28,6 +28,9 @@ def main():
 	print("If not, please leave the following input field blank and just input an ENTER!")
 	sep()
 	dir_name = input("project name please:").strip()
+
+	is_python = True # Default: python project
+	file_suffix = '.py'
 
 	is_config_exist = True
 	if not dir_name == "":
@@ -44,6 +47,9 @@ def main():
 						k = sets[0].strip(' ')
 						v = sets[1].strip(' ')
 						if k == 'name':
+							if v.__contains__('.m'):
+								is_python = False
+								file_suffix = '.m'
 							script_name = v.replace('.py','').replace('.m','')
 							judger[0] = True
 						if k == 'max_num':
@@ -84,11 +90,16 @@ def main():
 		sep()
 		print("STEP 3 : Tell us the entrance script name that will start your simulation")
 		sep()
-		script_name = input("main script name please (without .py suffix):").strip(" ").replace(".py","")
+		script_name = input("main script name please (WITH suffix .py or .m):").strip(" ")
+		if script_name.__contains__('.m'):
+			is_python = False
+			file_suffix = '.m'
+		script_name = script_name.replace('.py','').replace('.m','')
 
 		print()
 		sep()
 		print("STEP 4 : Open the main script and delete the loops for testing different parameters")
+		print("If you're using Matlab, please add `exit;` to the end of the script, otherwise it won't be functional!!")
 		sep()
 		input("PRESS ANY KEY when you finish...")
 
@@ -98,15 +109,15 @@ def main():
 		print("e.g. A=a(i), i is the original loop index => A=#p.1#")
 		print("e.g. B=b(j), j is the original loop index => B=#p.2#")
 		print("If the set of simulation parameter contains 3 parameters, you'd have to modify them sequentially to #p.1#, #p.2#, and #p.3#")
-		print("Please make sure your indentation is correct and #p.x# is written in lowercase!")
-		print("After you finish, the Python scripts will not be formatted correctly!")
+		print("If you're using Python, please make sure your indentation is correct and #p.x# is written in lowercase!")
+		print("After you finish, the Python or Matlab scripts will be tested several error syntax!")
 		sep()
 		input("PRESS ANY KEY when you finish...")
 
 		print()
 		print("Reading the number of simulation parameters...")
 		r = re.compile(r'#p.[0-9]*#')
-		with open(dir_name + "\\" + script_name + ".py", 'r', encoding="UTF-8") as f:
+		with open(dir_name + "\\" + script_name + file_suffix, 'r', encoding="UTF-8") as f:
 			lines = f.readlines()
 			placeholders = r.findall(','.join(lines))
 			max_paras = 0
@@ -120,7 +131,7 @@ def main():
 		sep()
 		para_list = list()
 		for n in range(max_paras):
-			pl = input("Give your value sets for para.%d : " % (n+1))
+			pl = input("Give your value sets for para.%d : " % (n+1)).strip(' ')
 			pl = pl.split(',')
 			para_list.append(pl)
 
@@ -141,10 +152,17 @@ def main():
 
 	print()
 	sep()
-	print("FINAL STEP : Please wait for us to run your stupid codes, will you?")
+	print("FINAL STEP : Please wait for me to run your stupid codes, will you?")
 	sep()
 	print("Settings for your simulation:")
-	print("project name: %s and main script name: %s" % (dir_name,script_name))
+	if is_python:
+		print("You've planned a parallel simulation for a Python project!")
+	else:
+		print("You've planned a parallel simulation for a Matlab project!")
+		with open(dir_name + "\\" + script_name + file_suffix, 'r', encoding="UTF-8") as f:
+			if not ','.join(f.readlines()).__contains__('exit'):
+				print("The last line of your script has not been added the command: `exit;` !!! PLEASE DO IT NOW !!!")
+	print("project name: %s , main script name: %s" % (dir_name,script_name))
 	print("script has %d simulation parameters, which are:" % max_paras)
 	c = 1
 	for p in para_list:
@@ -154,27 +172,29 @@ def main():
 	print("output folder: %s" % output_folder)
 	input("PRESS ANY KEY when you're ready to start the `parallel` simulation...")
 
+	# START Simulation !
 	print("Creating temporary directories for parallel simulations...")
 	plist_tmp = lists_combination(para_list, code=',')
 	plist = list()
 	for p in plist_tmp:
 		plist.append(p.split(','))
 	shutil.rmtree(dir_name + '_sim', ignore_errors=True)
-	time.sleep(3.0)
 	while os.path.exists(dir_name + '_sim'):
+		time.sleep(2.0)
 		pass
 	full_target_queue = queue.Queue()
+	sepper = '***'
 	for name in plist:
 		new_dir_name = dir_name + '_sim' + '\\' + dir_name + '_' + '_'.join(name)
 		full_target_queue.put(new_dir_name)
 		shutil.copytree(dir_name, new_dir_name)
-		with open(new_dir_name + '\\' + script_name + '.py', 'r', encoding="UTF-8") as f:
-			script = '**'.join(f.readlines())
+		with open(new_dir_name + '\\' + script_name + file_suffix, 'r', encoding="UTF-8") as f:
+			script = sepper.join(f.readlines())
 			for ind in range(max_paras):
 				rep_target = "#p." + str(ind+1) + "#"
 				script = script.replace(rep_target, name[ind])
-			script = script.split('**')
-		with open(new_dir_name + '\\' + script_name + '.py', 'w', encoding="UTF-8") as f:
+			script = script.split(sepper)
+		with open(new_dir_name + '\\' + script_name + file_suffix, 'w', encoding="UTF-8") as f:
 			f.writelines(script)
 	print("Created successfully...")
 
@@ -182,6 +202,7 @@ def main():
 	threadpool = dict()
 	for i in range(max_num):
 		threadpool[str(i)] = None
+	task_count = 0
 	while True:
 		# Check Thread Status & Pop dead threads
 		threadpool = clear_threadpool(threadpool, output_folder, dir_name)
@@ -192,19 +213,25 @@ def main():
 		while len_threadpool(threadpool) < max_num and not full_target_queue.empty():
 			if not full_target_queue.empty():
 				pp = full_target_queue.get()
-				t = threading.Thread(target=run_proc, args=(str(pp).replace('\\','/'),script_name + '.py'))
+				t = threading.Thread(target=run_proc, args=(str(pp).replace('\\','/'),script_name + file_suffix))
 				t.start()
 				threadpool = fill_threadpool(str(pp), t, threadpool)
-				print("Added simulation: %s ..." % pp)
+				task_count = task_count + 1
+				print("[ %d / %d ] Added simulation: %s ..." % (task_count,len(plist_tmp),pp))
 		time.sleep(5.0)
 	print("Simulations are all complete...")
 	print("Thanks for using Stupid Parallel Simulation !!!")
 	input("Press any key to close me...")
 
 
+# Other functions
+
 def run_proc(dirname,filename):
-	os.system("cd %s & start /wait cmd /c python %s" % (dirname, filename))
-	#"cd CNVChain_sim\CNVChain_0.1_3 & start /wait cmd /c python main_bran_implementation_blocktime.py"
+	if str(filename).strip(' ').endswith('.py'):
+		os.system("cd %s & start /wait cmd /c python %s" % (dirname, filename))
+		#"cd CNVChain_sim\CNVChain_0.1_3 & start /wait cmd /c python main_bran_implementation_blocktime.py"
+	else:
+		os.system("cd %s & start /wait cmd /c matlab -nosplash -nodesktop -wait -r %s" % (dirname, str(filename).replace('.m','')))
 
 def sep():
 	print("====================================")
---
+++ testm/test.m
@@ -0,0 +1,5 @@
+a = #p.1#
+b = #p.2#
+disp('test matlab project!!!');
+fid1=fopen(['results\re_' num2str(a) '_' num2str(b) '.txt'],'w');
+fprintf(fid1,' %f %f \n',a,b);
